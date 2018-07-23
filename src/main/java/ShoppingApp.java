dae8e0d0d3a67fc08b194569679599fe75e8aec3
upsd <up648421@myport.ac.uk>
2018-07-23 17:12:19 +0100

    Tidy-up application.

--- src/main/java/ShoppingApp.java
+++ src/main/java/ShoppingApp.java
@@ -7,22 +7,38 @@ public class ShoppingApp {
 
     public static void main(String[] args) {
 
-        Database database = null;
+        Database database = getDatabase();
 
-        if (System.getenv("SHOPPING_ENV").equals("test")) {
-            database = new InMemoryProductDatabase();
-            database.add(new Product("Breaking Bad", new Money(new BigDecimal(45))));
-            database.add(new Product("Fixing Good", new Money(new BigDecimal(24))));
-        }
+        addExampleProducts(database);
 
         ProductController productController = new ProductController(database, new JsonFormatter());
 
         staticFileLocation("/public");
+
         get("/", (req, res) -> null);
 
         get("/products", (req, res) -> {
             res.type("application/json");
             return productController.get();
         });
+    }
+
+    private static void addExampleProducts(Database database) {
+        database.add(new Product("Breaking Bad", new Money(new BigDecimal(45))));
+        database.add(new Product("Fixing Good", new Money(new BigDecimal(24))));
+    }
+
+    private static Database getDatabase() {
+        if (System.getenv("SHOPPING_ENV").equals("test")) {
+            return new InMemoryProductDatabase();
+        }
+
+        return new PostgreSQLDatabase(
+                System.getenv("SHOP_DB_HOST"),
+                5432,
+                System.getenv("SHOP_DB_NAME"),
+                System.getenv("SHOP_DB_USER"),
+                System.getenv("SHOP_DB_PASS")
+        );
     }
 }
