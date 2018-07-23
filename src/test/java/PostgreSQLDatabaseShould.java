29434ce6ad2907dd4ce3f098961de5d26f8b68b1
upsd <up648421@myport.ac.uk>
2018-07-23 16:42:53 +0100

    Use an in-memory database instead of using Docker.

--- src/test/java/PostgreSQLDatabaseShould.java
+++ src/test/java/PostgreSQLDatabaseShould.java
@@ -1,38 +1,29 @@
-import org.junit.*;
-import org.testcontainers.containers.PostgreSQLContainer;
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
+import com.opentable.db.postgres.junit.EmbeddedPostgresRules;
+import com.opentable.db.postgres.junit.SingleInstancePostgresRule;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class PostgreSQLDatabaseShould {
 
-    @ClassRule
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:9.6.2")
-            .withDatabaseName("shopping-db")
-            .withUsername("username")
-            .withPassword("password");
-
-    @Before
-    public void setup() {
-        postgreSQLContainer.start();
-    }
-
-    @After
-    public void teardown() {
-        postgreSQLContainer.stop();
-    }
+    @Rule
+    public SingleInstancePostgresRule pg = EmbeddedPostgresRules.singleInstance();
 
     @Test
     public void get_all_products() {
+        EmbeddedPostgres inMemoryPostgres = pg.getEmbeddedPostgres();
         PostgreSQLDatabase postgreSQLDatabase = new PostgreSQLDatabase(
-                postgreSQLContainer.getContainerIpAddress(),
-                postgreSQLContainer.getFirstMappedPort(),
-                postgreSQLContainer.getDatabaseName(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
+                "localhost",
+                inMemoryPostgres.getPort(),
+                "postgres",
+                "postgres",
+                "postgres"
         );
 
         postgreSQLDatabase.add(new Product("Breaking Bad", new Money(new BigDecimal(9))));
